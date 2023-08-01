45413b57c20a85f2ecf2e884e14f03eef0aa0c5c
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-08-02 00:02:26 +0600

    feat: added MySQL reposotory

---
+++ src/Repository/Class/MySQLArticleRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mazed\DesignPatterns\Repository\Class;
+
+class MySQLArticleRepository implements ArticleRepositoryInterface
+{
+    // Database connection or ORM instance goes here
+
+    public function findById($id): array
+    {
+        return [];
+        // Fetch Article by id from the database and return as an Article object
+    }
+
+    public function findAll(): array
+    {
+        return true;
+
+        // Fetch all Articles from the database and return as an array of Article objects
+    }
+
+    public function save(): bool
+    {
+        return true;
+        // Save or update the Article in the database and return success status
+    }
+
+    public function delete(): bool
+    {
+        // Delete the Article from the database and return success status
+    }
+}
